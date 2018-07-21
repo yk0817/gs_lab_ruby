22cfe460e2a7973220c17996eb02c08f5f53ae25
山本　憲 <em072029@gmail.com>
2018-07-21 13:51:55 +0900

    【第一週目】環境構築

---
+++ 1st_week/common.rb
@@ -0,0 +1,20 @@
+require 'nokogiri'
+require 'active_record'
+require 'open-uri'
+
+# 標準出力ログを出す
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+# active_recordを継承する。
+class Headline < ActiveRecord::Base
+end
+
+ActiveRecord::Base.establish_connection(
+  adapter:  "mysql2",  #アダプター名
+  host:     "localhost",  #ホスト名
+  username: "root",  #ユーザー名
+  password: "",  #パスワード
+  database: "ruby_study",  #データベース名
+)
+
+# p Headline.all
