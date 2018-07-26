ebb49dc7b891f44eefe6be750204c0737eed97cc
山本　憲 <em072029@gmail.com>
2018-07-26 19:31:47 +0900

    add common.rb

--- 1st_week/common.rb
+++ 1st_week/common.rb
@@ -1,20 +1,21 @@
 require 'nokogiri'
 require 'active_record'
 require 'open-uri'
+require 'pp'
 
-# 標準出力ログを出す
-ActiveRecord::Base.logger = Logger.new(STDOUT)
+# # 標準出力ログを出す
+# ActiveRecord::Base.logger = Logger.new(STDOUT)
+#
+# # active_recordを継承する。
+# class Headline < ActiveRecord::Base
+# end
+#
+# ActiveRecord::Base.establish_connection(
+#   adapter:  "mysql2",  #アダプター名
+#   host:     "localhost",  #ホスト名
+#   username: "root",  #ユーザー名
+#   password: "",  #パスワード
+#   database: "ruby_study",  #データベース名
+# )
 
-# active_recordを継承する。
-class Headline < ActiveRecord::Base
-end
-
-ActiveRecord::Base.establish_connection(
-  adapter:  "mysql2",  #アダプター名
-  host:     "localhost",  #ホスト名
-  username: "root",  #ユーザー名
-  password: "",  #パスワード
-  database: "ruby_study",  #データベース名
-)
-
-# p Headline.all
+p Nokogiri.class
