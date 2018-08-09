8c700441b7cf354d1f21f6bcef0c730f0cccc083
山本　憲 <em072029@gmail.com>
2018-08-09 21:29:32 +0900

    add example active support　

--- 2nd_week/date_active_support.rb
+++ 2nd_week/date_active_support.rb
@@ -1,13 +1,15 @@
 require './common'
 
-# 明日の日付
-p Date.today.tomorrow.strftime("%Y-%m-%d")
+# # 明日の日付
+# p Date.today.tomorrow.strftime("%Y-%m-%d")
+#
+# # 昨日の日付
+# p Date.today.yesterday.strftime("%Y-%m-%d")
+#
+# # 月初
+# p Date.today.beginning_of_month.strftime("%Y-%m-%d")
+#
+# # 月末
+# p Date.today.end_of_month.strftime("%Y-%m-%d")
 
-# 昨日の日付
-p Date.today.yesterday.strftime("%Y-%m-%d")
-
-# 月初
-p Date.today.beginning_of_month.strftime("%Y-%m-%d")
-
-# 月末
-p Date.today.beginning_of_month.strftime("%Y-%m-%d")
+p 'User'.tableize 
