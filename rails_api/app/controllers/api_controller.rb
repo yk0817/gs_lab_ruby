ff2eb3dd01024c19e19880c15cb4e0e69be16408
山本　憲 <em072029@gmail.com>
2018-08-15 07:03:26 +0900

    apiリクエスト

--- rails_api/app/controllers/api_controller.rb
+++ rails_api/app/controllers/api_controller.rb
@@ -1,6 +1,16 @@
 class ApiController < ApplicationController
+  # before_action :valid_parameters
+  include BeforeAction
   def index
     @headlines = Headline.all
     render json: @headlines
   end
+
+  # private
+  #   def valid_parameters?
+  #     unless params[:id]
+  #       @errors = [["bad_request", 500]]
+  #       render json: @errors, status: 500
+  #     end
+  #   end
 end
