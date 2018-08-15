89f5d1a5a7411d75333c3e79e60a9ea458c2bc45
山本　憲 <em072029@gmail.com>
2018-08-15 23:20:27 +0900

    controller　色々なレスポンス

--- rails_api/app/controllers/api_controller.rb
+++ rails_api/app/controllers/api_controller.rb
@@ -1,16 +1,30 @@
 class ApiController < ApplicationController
-  # before_action :valid_parameters
-  include BeforeAction
+  before_action :valid_parameters?
+  # include BeforeAction
   def index
     @headlines = Headline.all
-    render json: @headlines
+    # render json: @headlines
+    # render :status => 200
+    send_data(
+      csv_data,
+      type: 'text/csv; charset=utf-8',
+      filename: "test.csv"
+    )
   end
 
-  # private
-  #   def valid_parameters?
-  #     unless params[:id]
-  #       @errors = [["bad_request", 500]]
-  #       render json: @errors, status: 500
-  #     end
-  #   end
+  private
+    def valid_parameters?
+      unless params[:id]
+        @errors = [["bad_request", 500]]
+        render json: @errors, status: 500
+      end
+    end
+
+    # 勉強用
+    require 'csv'
+    def csv_data
+      CSV.generate(row_sep: "\r\n") do |csv|
+        csv << ['hoge']
+      end
+    end
 end
