bb965743c57159d7a7411e2a9d8d747067a43f9e
山本　憲 <em072029@gmail.com>
2018-08-19 15:29:01 +0900

    add controller test

--- rails_api/spec/controllers/api_controller_spec.rb
+++ rails_api/spec/controllers/api_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe ApiController, type: :controller do
-  
+  describe "GET #index" do
+    context "GET with params id: 1" do
+      let(:params){{ id: 1 }}
+      subject { get :index, params: params }
+      it "should be success" do
+        expect(subject).to be_successful
+        expect(subject.status).to eq 200
+      end
+    end
+
+    context "GET without params " do
+      subject { get :index }
+      it "should not be success" do
+        expect(subject).not_to be_successful
+        expect(subject.status).to eq 500
+      end
+    end
+  end
 end
