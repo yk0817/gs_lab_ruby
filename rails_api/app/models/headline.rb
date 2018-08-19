50faf389743f681ca219cfe6c89ce6f61688ad5c
山本　憲 <em072029@gmail.com>
2018-08-19 12:45:22 +0900

    add validation

--- rails_api/app/models/headline.rb
+++ rails_api/app/models/headline.rb
@@ -1,4 +1,14 @@
 class Headline < ApplicationRecord
   belongs_to :school_blog
   has_one :article, :dependent => :destroy
+
+  validates :title, uniqueness: true
+  validates :title, length: { maximum: 6 }
+  validate  :custom_validate
+
+  def custom_validate
+    if title.match(/validate_test/)
+      errors.add(:title, "validate_error")
+    end
+  end
 end
